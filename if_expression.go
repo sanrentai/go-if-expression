a75ba157a04af2e6fd9ae360e177249747f86c0a
Sany <sanrentai@163.com>
2023-03-08 10:58:25 +0800

    修改注释和格式

--- if_expression.go
+++ if_expression.go
@@ -6,26 +6,24 @@ package if_expression
 //	@param boolExpression: 布尔表达式，最终返回一个布尔值
 //	@param trueReturnValue: 当boolExpression返回值为true的时候返回的值
 //	@param falseReturnValue: 当boolExpression返回值为false的时候返回的值
-//	@return bool: 三元表达式的结果，为trueReturnValue或者falseReturnValue中的一个
+//	@return T: 三元表达式的结果，为trueReturnValue或者falseReturnValue中的一个
 func Return[T any](boolExpression bool, trueReturnValue, falseReturnValue T) T {
 	if boolExpression {
 		return trueReturnValue
-	} else {
-		return falseReturnValue
-	}
+	} 
+	return falseReturnValue
 }
 
 // ReturnByFunc
 //
 //	@Description: if实现的三元表达式
 //	@param boolExpression: 布尔表达式，最终返回一个布尔值
-//	@param trueReturnValue: 当boolExpression返回值为true的时候执行此函数并返回值
-//	@param falseReturnValue: 当boolExpression返回值为false的时候执行此函数并返回值
-//	@return bool: 三元表达式的结果，为trueReturnValue或者falseReturnValue中的一个
+//	@param trueFuncForReturnValue: 当boolExpression返回值为true的时候执行此函数并返回值
+//	@param falseFuncForReturnValue: 当boolExpression返回值为false的时候执行此函数并返回值
+//	@return T: 三元表达式的结果，为trueReturnValue或者falseReturnValue中的一个
 func ReturnByFunc[T any](boolExpression bool, trueFuncForReturnValue, falseFuncForReturnValue func() T) T {
 	if boolExpression {
 		return trueFuncForReturnValue()
-	} else {
-		return falseFuncForReturnValue()
 	}
+	return falseFuncForReturnValue()
 }
